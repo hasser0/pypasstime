0496051b81a941017fbf1f76dd32a629bc6fc205
hasser0 <osvaldoes.unam@gmail.com>
2024-07-18 16:14:33 -0600

    command line functionality

--- src/pypasstime/__init__.py
+++ src/pypasstime/__init__.py
@@ -1,4 +1,7 @@
 from datetime import datetime
 
+from .puzzle import create_puzzle  # noqa: F401
+from .puzzle import solve_puzzle  # noqa: F401
+
 
 __version__ = datetime.today().strftime("%Y.%m.%d")
--- src/pypasstime/puzzle.py
+++ src/pypasstime/puzzle.py
@@ -1,6 +1,8 @@
+import pickle
 import sys
 from random import randint
 
+import click
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.asymmetric import rsa
 
@@ -21,10 +23,15 @@ def generate_time_lock_puzzle(message, n_seconds):
     a = randint(2, n - 1)
     e = pow(2, t, phi_n)
     ck = k + pow(a, e, n)
-    return n, a, t, ck, cm
+    return {"n": n, "a": a, "t": t, "ck": ck, "cm": cm}
 
 
-def solve_time_lock_puzzle(n, a, t, ck, cm):
+def solve_time_lock_puzzle(puzzle):
+    n = puzzle["n"]
+    a = puzzle["a"]
+    t = puzzle["t"]
+    ck = puzzle["ck"]
+    cm = puzzle["cm"]
     b = a % n
     for _ in range(t):
         b = b**2 % n
@@ -33,4 +40,20 @@ def solve_time_lock_puzzle(n, a, t, ck, cm):
     return decypher.decrypt(cm).decode("utf-8")
 
 
-# TODO create and read from zip file and using click
+@click.command()
+@click.argument("filename", type=click.File("rb"))
+def solve_puzzle(filename):
+    puzzle = pickle.load(filename)
+    solution = solve_time_lock_puzzle(puzzle)
+    with open("solved_puzzle.txt", "w") as f:
+        f.write(solution)
+
+
+@click.command()
+@click.option("-f", "--filename", type=click.File("r"))
+@click.option("-t", "--text", type=str)
+@click.option("-s", "seconds", type=int)
+def create_puzzle(filename, text, seconds):
+    puzzle = generate_time_lock_puzzle(text, seconds)
+    with open("puzzle.pkl", "wb") as pkl:
+        pickle.dump(puzzle, pkl)
